de6c880fcc79b721dd1027fecccff0ac26b48bc4
CindyOrtega416 <37404924+CindyOrtega416@users.noreply.github.com>
2022-08-19 12:53:44 -0300

    Modified linkedin link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import {Col, Container, Row} from "react-bootstrap";
 import logo from "../assets/img/logo.svg"
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/github-icon.svg';
-import navIcon3 from '../assets/img/telegram-icon.svg';
+import navIcon3 from '../assets/img/telegram-icon.svg';a
 
 export function Footer() {
     return (
@@ -15,7 +15,7 @@ export function Footer() {
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/cindy-micaela-ortega-palma-6a249a171/"><img src={navIcon1} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/cindyortegapalma"><img src={navIcon1} alt="" /></a>
                             <a href="https://github.com/CindyOrtega416"><img src={navIcon2} alt="" /></a>
                             <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcSGLPzGMHkpQXmGGMrgjKthNSCcPjCwpBzDhqzLkvpgThcfNHKGDpdbQsHlqfKDlNWvHbSGf"><img src={navIcon3} alt="" /></a>
                         </div>
